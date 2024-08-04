0ea7c295ed43c563af316d516d013b03b3a9b94a
Xertis <118364459+Xertis@users.noreply.github.com>
2024-08-04 08:59:36 +0300

    add table_utils

---
+++ modules/utils/table_utils.lua
@@ -0,0 +1,57 @@
+local table_utils = {}
+
+function table_utils.easy_concat(tbl)
+    local output = ""
+    for i, value in pairs(tbl) do
+        output = output .. tostring(value)
+        if i ~= #tbl then
+            output = output .. ", "
+        end
+    end
+    return output
+end
+
+function table_utils.equals(tbl1, tbl2)
+    return table_utils.easy_concat(tbl1) == table_utils.easy_concat(tbl2)
+end
+
+function table_utils.tbl_in_tbl(tbl, key)
+    local temp_table = {}
+    for _, b in ipairs(tbl) do
+        temp_table[table_utils.easy_concat(b)] = b
+    end
+    key = table_utils.easy_concat(key)
+    return temp_table[key]
+end
+
+function table_utils.find(table, element)
+    for _, value in pairs(table) do
+        if value == element then
+            return true
+        end
+    end
+    return false
+end
+
+function table_utils.insert_unique(tbl, elem)
+    if tbl ~= nil then
+        if table_utils.find(tbl, elem) == false then
+            table.insert(tbl, elem)
+        end
+    end
+end
+
+function table_utils.copy(t)
+    local t2 = {}
+    for k,v in pairs(t) do
+        t2[k] = v
+    end
+    return t2
+end
+
+function table_utils.sort(t)
+    table.sort(t, function(a, b) return a < b end)
+    return t
+end
+
+return table_utils
