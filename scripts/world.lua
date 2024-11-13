5219ae99fa619614b4a4ae9511e5bd1aa6bd2f6c
Xertis <xertis3785@gmail.com>
2024-11-13 19:25:54 +0300

    add drop

--- scripts/world.lua
+++ scripts/world.lua
@@ -5,3 +5,11 @@ require("noname:player/blocked")
 function on_world_tick( ... )
     BlockedFunc.update()
 end
+
+function on_block_broken(id, x, y, z)
+    x, y, z = math.floor(x), math.floor(y), math.floor(z)
+    entities.spawn("base:drop", {x+0.5, y+0.5, z+0.5}, {base__drop={
+        id=id,
+        count=1
+    }})
+end
