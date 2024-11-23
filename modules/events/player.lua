8af43153a478b65e98a5782aa769bc8390018820
Xertis <xertis3785@gmail.com>
2024-11-23 12:38:30 +0300

    add player drop

--- modules/events/player.lua
+++ modules/events/player.lua
@@ -24,6 +24,19 @@ end
 
 function module.death(pid)
     if player_bars.get_hp() < 1 then
+        local inv = player.get_inventory(pid)
+        local size = inventory.size(inv)
+
+        for slot=0, size-1 do
+            local id, count = inventory.get(inv, slot)
+            local x, y, z = player.get_pos(pid)
+            x, y, z = math.floor(x), math.floor(y), math.floor(z)
+            entities.spawn("base:drop", {x+0.5, y+0.5, z+0.5}, {base__drop={
+                id=id,
+                count=count
+            }})
+            inventory.set(inv, slot, 0, 0)
+        end
         player.set_pos(pid, unpack({player.get_spawnpoint(pid)}))
         player_bars.set_hp(100)
     end
